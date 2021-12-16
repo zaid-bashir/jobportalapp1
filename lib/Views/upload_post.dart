9dc4fed308d8235239683647ad567bcea9be39fb
tahseenabass <tahseenabass@clusttech.com>
2021-12-16 09:20:47 +0530

    job page created

--- lib/Views/upload_post.dart
+++ lib/Views/upload_post.dart
@@ -1,22 +1,69 @@
-// import 'package:flutter/material.dart';
-//
-//
-//
-// class UploadPost extends StatefulWidget {
-//   const UploadPost({Key key}) : super(key: key);
-//
-//   @override
-//   _UploadPostState createState() => _UploadPostState();
-// }
-//
-// class _UploadPostState extends State<UploadPost> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       child: Container(
-//
-//       ),
-//
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class UploadPost extends StatefulWidget {
+  const UploadPost({Key key}) : super(key: key);
+
+  @override
+  _UploadPostState createState() => _UploadPostState();
+}
+
+class _UploadPostState extends State<UploadPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  // backgroundImage: AssetImage("assets/show_image"),
+                  backgroundColor: Colors.blue,
+                ),
+
+                const SizedBox(
+                  width: 5,
+                ),
+                Column(
+                  children: [
+                    Text(
+                       "Tahseen abass",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Chip(
+                        avatar: CircleAvatar(
+                            backgroundColor: Colors.grey, child: const Text("umar")),
+                        label: const Text("anyone")),
+                  ],
+                ),
+
+
+              ],
+            ),
+
+            SizedBox(
+              height: 15
+            ),
+            TextField(
+
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+
+                label: Text("what do you want to talk about"),
+              ),
+
+
+
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
