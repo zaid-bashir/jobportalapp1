5ce2e329e307ea0744bbffc282260bc4b253ce29
umarmehraj <umarmehraj@clusttech.com>
2021-12-27 09:40:20 +0530

    feat:  updated ui

--- lib/Views/SignIn/keyskills.dart
+++ lib/Views/SignIn/keyskills.dart
@@ -1,9 +1,6 @@
-import 'package:dropdown_search/dropdown_search.dart';
+
 import 'package:flutter/material.dart';
-import 'package:getwidget/colors/gf_color.dart';
-import 'package:getwidget/components/dropdown/gf_multiselect.dart';
-import 'package:getwidget/types/gf_checkbox_type.dart';
-import 'package:job_portal/Views/SignIn/careerpreference.dart';
+
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:multi_select_flutter/util/multi_select_list_type.dart';
