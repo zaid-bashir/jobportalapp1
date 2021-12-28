118fa57d21e006bf42118958fa3ae11c491c091a
tahseenabass <tahseenabass@clusttech.com>
2021-12-28 13:04:48 +0530

    created model for register new account

--- lib/Views/Home/model/registernewaccount.dart
+++ lib/Views/Home/model/registernewaccount.dart
@@ -19,38 +19,51 @@ class RegisterNewAccount {
 
 
   RegisterNewAccount({
-    int titleId,
-    String titleName,
-    String firstName,
-    String middleName,
-    String lastName,
-    String email,
-    String gender,
-    String experience,
-    int yearsId,
-    String yearsName,
-    int monthsId,
-    String monthsName,
-    int jobCategoryId,
-    String jobCategoryName,
-    int currentLocationId,
-    String currentLocationName
+    this.titleId,
+  this.titleName,
+    this.firstName,
+    this. middleName,
+    this.lastName,
+    this.email,
+    this.gender,
+    this.experience,
+    this.yearsId,
+    this.yearsName,
+    this.monthsId,
+    this.monthsName,
+    this.jobCategoryId,
+    this.jobCategoryName,
+    this.currentLocationId,
+    this.currentLocationName
 
   });
 
 
 // changes
-  RegisterNewAccount({this.titleId, this.titleName});
-
-  RegisterNewAccount .add({this.titleName});
 
   factory RegisterNewAccount .fromJson(Map<String, dynamic> item) {
     return RegisterNewAccount(
-        titleId: item["titleId"], titleName: item["titleName"]);
+        titleId: item["working"],
+        titleName: item["  companyId"],
+        firstName: item["companyName"],
+        middleName: item["  designationId"],
+        lastName: item["  designationName"],
+        email: item["salary"],
+        gender: item["  years"],
+        experience: item["industryId"],
+        yearsId: item["  industryName"],
+        yearsName: item["  compId"],
+        jobCategoryId: item["  compName"],
+        jobCategoryName: item["designId"],
+        currentLocationId: item["  designName"],
+        currentLocationName: item["sal"]
+
+    );
   }
 
   Map<String, dynamic> toJson() {
-    return {"titleId": titleId,
+    return {
+      "titleId": titleId,
       "titleName": titleName,
 
       "firstName" :firstName,
