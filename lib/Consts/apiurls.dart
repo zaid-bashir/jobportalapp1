43f7b367fbd07268a55b4d4e312b330692080790
zaidbashir <zaidbashir@clusttech.com>
2021-12-29 16:56:06 +0530

    Commited one change

--- lib/Consts/apiurls.dart
+++ lib/Consts/apiurls.dart
@@ -1,6 +1,9 @@
 class ApiUrls{
   static String kgetOTP = "http://192.168.0.20:7038/jobportal-app/api/generate-otp";
   static String kverifyOTP = "http://192.168.0.20:7038/jobportal-app/api/validate-otp";
+  static String kPassingYear = "http://192.168.0.20:7038/jobportal-app/app/api/populate/year";
+  static String kGradingSystem = "http://192.168.0.20:7038/jobportal-app/app/api/populate/grading-system";
+  static String kShift = "http://192.168.0.20:7038/jobportal-app/app/api/populate/shift";
   static String ktitles = "http://192.168.0.20:7038/jobportal-app/app/api/populate/title";
   static String kgender = "http://192.168.0.20:7028/jobportal-app/app/api/populate/gender";
 
